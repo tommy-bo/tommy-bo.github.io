b0e48f2a6c2e364de011858a60a11036224f897e
Tommy Bo <tommy.bo@visma.com>
2014-09-04 02:40:26 +0200

    Lagt til oppgaver

---
+++ testing-in-practice/oppgaver-slide-setup.js
@@ -0,0 +1,27 @@
+var applyLayout = function(setup) {
+		setup
+				.first()
+						.moveY(1000)
+				.next()
+						.moveY(1000)
+				.next()
+						.moveY(1000)
+				.next()
+						.moveY(1000)
+				.next()
+						.moveY(1000)
+				.next()
+						.moveY(1000)
+				.next()
+						.moveY(1000)
+				.apply();
+};
+
+
+window.onload = function(e) {
+		var steps = document.getElementsByClassName("step");
+		applyLayout(new SlideSetup(steps));
+		
+		impress().init();
+}
+
